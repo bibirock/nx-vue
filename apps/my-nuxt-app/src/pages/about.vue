874276e9742603df09a133b8e4ca1019a341f00b
Joe.Chen <joechen@tracle-tw.com>
2024-04-16 17:50:45 +0800

    init

---
+++ apps/my-nuxt-app/src/pages/about.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { One } from "@my-nuxt-workspace/my-vue-lib-1"
+import { Two } from "@my-nuxt-workspace/my-vue-lib-2"
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page.</h1>
+    <One />
+    <Two />
+  </div>
+</template>
+
+<style>
+@media (min-width: 768px) {
+  .about {
+    max-width: 768px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 0 1rem;
+  }
+}
+</style>
---
+++ libs/my-vue-lib-2/src/lib/two/two.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Three } from "@my-nuxt-workspace/my-vue-lib-3"
+</script>
+
+<template>
+  <p>Welcome to Two!</p>
+  <Three />
+</template>
+
+<style scoped></style>
